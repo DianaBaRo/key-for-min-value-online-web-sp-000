025b82d980494d75480aaa9777bd01179918671b
Diana Badas <diana.badas@gmail.com>
2018-12-29 21:47:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
   minimum_value = 100000000
-  minimum_key = nil
+  minimum_key = ""
   name_hash.each do |item, value|
     if value < minimum_value
       minimum_value = value
